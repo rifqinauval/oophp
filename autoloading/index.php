31a7c16e329336e2134afef4f25a4a868bf78295
rifqinauval <173040080@mail.unpas.ac.id>
2019-10-09 21:37:07 +0700

    Autoloading

---
+++ autoloading/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'App/init.php';
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen", 300000, 100,);
+$produk2 = new Game("Naruto Storm 4", "Yohei Ishibashi", "Bandai Namco Entertainment", 500000, 50);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk( $produk1 );
+$cetakProduk->tambahProduk( $produk2 );
+echo $cetakProduk->cetak();
+
+echo "<hr>";
+
+new Coba();
+
+?>
